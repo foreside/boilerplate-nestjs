c594a66aa6f1f3ed5a4078fac8e6eb093c78629d
Niek Heezemans <niek.heezemans@gmail.com>
2022-02-08 09:23:04 +0100

    Fixed types for password_hash_algorithm_enum and password_status_enum on initial-database-schema migration

--- src/migrations/1641462638160-initial-database-schema.migration.ts
+++ src/migrations/1641462638160-initial-database-schema.migration.ts
@@ -15,6 +15,12 @@ export class initialDatabaseSchemaMigration1641462638160
       `CREATE UNIQUE INDEX "IDX_EMAIL" ON "user" ("email") `,
     );
     await queryRunner.query(
+      `CREATE TYPE "public"."password_hash_algorithm_enum" AS ENUM('argon2id', 'scrypt', 'bcrypt')`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."password_status_enum" AS ENUM('active', 'inactive')`,
+    );
+    await queryRunner.query(
       `CREATE TABLE "password" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "correlation_id" character varying NOT NULL, "password" character varying NOT NULL, "hash_algorithm" "public"."password_hash_algorithm_enum" NOT NULL DEFAULT 'argon2id', "status" "public"."password_status_enum" NOT NULL DEFAULT 'inactive', "options" text NOT NULL, "archived" boolean NOT NULL DEFAULT false, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "record_version" integer NOT NULL, "user_id" uuid, CONSTRAINT "PK_cbeb55948781be9257f44febfa0" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
@@ -26,6 +32,10 @@ export class initialDatabaseSchemaMigration1641462638160
     console.log('Running initialDatabaseSchemaMigration1641462638160 DOWN');
     await queryRunner.query(
       `ALTER TABLE "password" DROP CONSTRAINT "FK_4cd77c9b2e2522ee9d3671b3bc1"`,
+    );
+    await queryRunner.query(`DROP TYPE "public"."password_status_enum"`);
+    await queryRunner.query(
+      `DROP TYPE "public"."password_hash_algorithm_enum"`,
     );
     await queryRunner.query(`DROP TABLE "password"`);
     await queryRunner.query(`DROP INDEX "public"."IDX_EMAIL"`);
